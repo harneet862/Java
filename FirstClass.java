e12e65aef5b4c780e820e44dfe631ed8d9be93cb
Harneet Tatla <harneetk@cs.ualberta.ca>
2024-07-01 12:18:39 -0600

    Introduction

---
+++ FirstClass.java
@@ -0,0 +1,27 @@
+class FirstClass {
+    public static void main(String args[]) {
+        System.out.println("Hello World");    
+    }
+}
+
+// How is my code running ??
+/*
+i) Compilation:
+Whatever we wrote is called source code and it goes to compiler in JDK and it is converted into byte
+code with the extension .class and this byte code can be ran on any operating system as long as the 
+operating system has JRE which is Java Run time environment due to which it is very portable 
+ii) Execution:
+The byte code which we got JVM gets it and converts it into native code
+
+Components of program:
+1. Function:
+    void main(){
+    }
+2. Class:
+    Class main{
+        void main(){
+        }
+    }
+ * 
+ */
+
