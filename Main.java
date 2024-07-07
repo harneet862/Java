b750e9df0922d2f46623f2c453b3e4d9ee743990
Harneet Tatla <harneetk@cs.ualberta.ca>
2024-07-07 13:25:24 -0600

    Input/ quit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+     // the newline can be generated with the help of '/n'
+     // the other method is to use system.out.print which doesnot print a newline
+     // this method prints the new line system.out.println which does print new line at the end 
+        System.out.println("My name is harneet");
+        System.out.print("Hello World With Javan\n");
+    }
+}
---
+++ RandomWord.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+
+class RandomWord {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String inputWord = scanner.nextLine();
+        System.out.print(scanner);
+    }
+}
