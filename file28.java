02cbda772923e11faa2b33e52d8ec678d41827b4
Harneet Tatla <harneetk@cs.ualberta.ca>
2024-09-21 14:26:06 -0600

    Patterns

--- file28.java
+++ file28.java
@@ -1,5 +1,12 @@
 public class file28 {
     public static void main(String args[]){
-        for (int i = 1; i < )
-    }
+        int count = 1;
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j < i+1; j++){
+                System.out.print(count);
+                count++;
+            }
+            System.out.print("\n");
+        }
+    }  
 }
